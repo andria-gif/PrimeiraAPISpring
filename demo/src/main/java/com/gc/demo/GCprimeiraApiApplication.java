c7658fc8dbcfd2cebd7efe0a777a9f1fede8cbc2
andria-gif <rosadiasandria@gmail.com>
2023-10-27 21:33:19 -0300

    primeira implementação

---
+++ demo/src/main/java/com/gc/demo/GCprimeiraApiApplication.java
@@ -0,0 +1,13 @@
+package com.gc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GCprimeiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GCprimeiraApiApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/gc/demo/GCprimeiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GCprimeiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
